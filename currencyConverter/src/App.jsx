923fe57bd09bd818856fc7ff06e62adb8e59b4c9
divyanshu jha <divyanshu3636@gmail.com>
2025-02-22 20:03:38 +0530

    miniContext

---
+++ currencyConverter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-3xl bg-green-600'>HII</h1>
+    </>
+  )
+}
+
+export default App
---
+++ miniContext/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React, {useContext,useState} from 'react'
+import UserContext from '../context/UserContext'
+function Profile() {
+    const {user}= useContext(UserContext);
+    if(!user) return <div>Please Login</div>
+  return (
+    <div>Welcome {user.userName}</div>
+  )
+}
+
+export default Profile
